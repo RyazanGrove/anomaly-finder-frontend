e9508d96fb82de276a6409091bcdcc7c51d18192
RyazanGrove <53538807+RyazanGrove@users.noreply.github.com>
2024-08-24 21:26:59 +0300

    add routing to components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ScoresComponent } from './components/scores/scores.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { RulesComponent } from './components/rules/rules.component';
+import { AboutComponent } from './components/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'main', component: LandingPageComponent },
+    { path: 'rules', component: RulesComponent },
+    { path: 'scores', component: ScoresComponent },
+    { path: 'about', component: AboutComponent },
+    { path: '', redirectTo: '/main', pathMatch: 'full' }, // Default route
+    { path: '**', redirectTo: '/main' } // Wildcard route for 404
+];
