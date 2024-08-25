6661099857009b4cd94fbe4c59d9f21bd4880b18
RyazanGrove <53538807+RyazanGrove@users.noreply.github.com>
2024-08-25 22:27:17 +0300

    add environment constants

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    baseUrl: 'http://localhost:8080/',
+    production: true,
+    imageInfoEndpoint: 'info/',
+    imageEndpoint: 'image/',
+    versionEndpoint: 'version/'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    baseUrl: 'http://localhost:8080/',
+    production: false,
+    imageInfoEndpoint: 'info/',
+    imageEndpoint: 'image/',
+    versionEndpoint: 'version/'
+};
