d8b0ba48cf6b9d78b76ba6fdeb1c62c9bbaa91fa
RyazanGrove <53538807+RyazanGrove@users.noreply.github.com>
2024-08-25 22:28:43 +0300

    implement loadImage in image service

--- src/app/services/image.service.ts
+++ src/app/services/image.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImageService {
+  private baseUrl = environment.baseUrl;
+  private imageEndpoint = environment.imageEndpoint;
 
-  constructor() { }
+  private http = inject(HttpClient)
+ 
+  getImageByUrl(imageUrl: string): Observable<Blob> {
+    return this.http.get(imageUrl, { responseType: 'blob' });
+  }
+
+  loadImage(imageId: number): Observable<string> {
+    const url = `${this.baseUrl}${this.imageEndpoint}${imageId}`;
+    return this.getImageByUrl(url).pipe(
+      map((blob: Blob) => URL.createObjectURL(blob))
+    );
+  }
 }
