7b44b4a62b9c878adc0a3203116549ca2cd8d97f
RyazanGrove <53538807+RyazanGrove@users.noreply.github.com>
2024-08-25 22:30:52 +0300

    implement loading image in image component

--- src/app/components/landing-page/components/image/image.component.ts
+++ src/app/components/landing-page/components/image/image.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ImageService } from '../../../../services/image.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-image',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './image.component.html',
   styleUrl: './image.component.scss'
 })
-export class ImageComponent {
+export class ImageComponent implements OnInit {
+  @Input() imageId: number = 1;
+  image: string | undefined;
 
+  constructor(private imageService: ImageService) { }
+
+  ngOnInit() {
+    this.imageService.loadImage(this.imageId).subscribe(image => {
+      this.image = image;
+    })
+  }
 }
