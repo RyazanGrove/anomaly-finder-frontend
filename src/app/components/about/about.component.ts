3c39245ce84fcd1bcc0f160c4db32a85d1d2a13e
RyazanGrove <53538807+RyazanGrove@users.noreply.github.com>
2024-08-26 15:28:07 +0300

    implement about component

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { VersionService } from '../../services/version.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
 
+  public frontendVersion!: string;
+  public backendVersion!: string;
+
+  constructor(private versionService: VersionService) { }
+
+  ngOnInit(): void {
+    this.frontendVersion = this.versionService.getFrontendVersion();
+    this.versionService.getBackendVersion().subscribe(
+      response => this.backendVersion = response
+    );
+  }
 }
