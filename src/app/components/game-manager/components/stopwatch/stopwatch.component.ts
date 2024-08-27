b0f2b4d2a84985fcfc853a99067c4eda0a6e74e9
RyazanGrove <53538807+RyazanGrove@users.noreply.github.com>
2024-08-27 23:54:24 +0300

    implement stopwatch component

--- src/app/components/game-manager/components/stopwatch/stopwatch.component.ts
+++ src/app/components/game-manager/components/stopwatch/stopwatch.component.ts
@@ -1,12 +1,52 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-stopwatch',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './stopwatch.component.html',
   styleUrl: './stopwatch.component.scss'
 })
 export class StopwatchComponent {
+  
+  minutes: number = 0;
+  seconds: number = 0;
+  private intervalId: any;
+  private running: boolean = false;
 
+  start() {
+    if (!this.running) {
+      this.running = true;
+      this.intervalId = setInterval(() => {
+        this.incrementTime();
+      }, 1000);
+    }
+  }
+
+  stop() {
+    if (this.running) {
+      this.running = false;
+      clearInterval(this.intervalId);
+    }
+  }
+
+  reset() {
+    this.stop();
+    this.minutes = 0;
+    this.seconds = 0;
+  }
+
+  resetAndStart() {
+    this.reset();
+    this.start();
+  }
+
+  private incrementTime() {
+    this.seconds++;
+    if (this.seconds === 60) {
+      this.seconds = 0;
+      this.minutes++;
+    }
+  }
 }
