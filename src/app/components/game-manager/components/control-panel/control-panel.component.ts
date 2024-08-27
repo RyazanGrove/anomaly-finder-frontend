a67612a790d03c609f13cf4d327d8af6ea724973
RyazanGrove <53538807+RyazanGrove@users.noreply.github.com>
2024-08-28 00:15:21 +0300

    implement control-panel component

--- src/app/components/game-manager/components/control-panel/control-panel.component.ts
+++ src/app/components/game-manager/components/control-panel/control-panel.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-control-panel',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './control-panel.component.html',
   styleUrl: './control-panel.component.scss'
 })
 export class ControlPanelComponent {
 
+  @Output() startGame: EventEmitter<void> = new EventEmitter();
+  @Output() nextImage: EventEmitter<void> = new EventEmitter();
+  @Output() finishGame: EventEmitter<void> = new EventEmitter();
+
+  startButtonIsDisabled: boolean = false;
+  nextImageButtonIsDisabled: boolean = false;
+  finishButtonIsDisabled: boolean = false;
+
+  startButtonPressed() {
+    this.startButtonIsDisabled = true;
+    this.startGame.emit();
+  }
+
+  nextImageButtonPressed() {
+    this.nextImageButtonIsDisabled = true;
+    this.finishGame.emit();
+  }
+
+  finishButtonPressed() {
+    this.finishGame.emit();
+  }
+
+  makeNextImageButtonActive() {
+    this.nextImageButtonIsDisabled = false;
+  }
+
+  makeFinishButtonActive() {
+    this.finishButtonIsDisabled = false;
+  }
 }
