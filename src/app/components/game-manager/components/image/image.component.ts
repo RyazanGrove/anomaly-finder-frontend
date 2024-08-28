09cfae2e20ede02a7c155de50f71a41858f9c224
RyazanGrove <53538807+RyazanGrove@users.noreply.github.com>
2024-08-28 18:25:32 +0300

    implement image component

--- src/app/components/game-manager/components/image/image.component.ts
+++ src/app/components/game-manager/components/image/image.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
 import { ImageService } from '../../../../services/image.service';
 import { CommonModule } from '@angular/common';
+import { ImageInfo } from '../../../../models/image-info';
 
 @Component({
   selector: 'app-image',
@@ -9,15 +10,48 @@ import { CommonModule } from '@angular/common';
   templateUrl: './image.component.html',
   styleUrl: './image.component.scss'
 })
-export class ImageComponent implements OnInit {
-  @Input() imageId: number = 1;
+export class ImageComponent implements OnChanges {
+
+  @Input() data!: ImageInfo;
+  @Output() targetFound: EventEmitter<void> = new EventEmitter();
+
   image: string | undefined;
+  clickCoords: { x: number, y: number } | null = null;
+
+  boundingRectLeft: number = 0;  
+  boundingRectTop: number = 0;  
 
   constructor(private imageService: ImageService) { }
 
-  ngOnInit() {
-    this.imageService.loadImage(this.imageId).subscribe(image => {
+  ngOnChanges(){
+    this.imageService.loadImage(this.data.fileName).subscribe(image => {
       this.image = image;
     })
+  }
+
+  onImageClick(event: MouseEvent): void {
+    const imageElement = event.target as HTMLImageElement;
+
+    const boundingRect = imageElement.getBoundingClientRect();
+    this.boundingRectLeft = boundingRect.left;
+    this.boundingRectTop = boundingRect.top;
+    const x = event.clientX - this.boundingRectLeft;
+    const y = event.clientY - this.boundingRectTop;
+
+
+    this.clickCoords = {
+      x: x,
+      y: y
+    }
+
+    this.checkForFoundTarget(x,y);
+  }
+
+  checkForFoundTarget(x: number, y: number){
+    const target = this.data.target;
+
+    if(target.xMin <= x && x <= target.xMax && target.yMin <= y && target.yMax){
+      this.targetFound.emit()
+    }
   }
 }
