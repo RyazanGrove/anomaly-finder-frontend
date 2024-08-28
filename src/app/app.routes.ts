c25231acc0469f1ab5ef21abb58e385b6a33d248
RyazanGrove <53538807+RyazanGrove@users.noreply.github.com>
2024-08-28 18:36:11 +0300

    add game-manager component in routing instead of landing-page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { ScoresComponent } from './components/scores/scores.component';
-import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { GameManagerComponent } from './components/game-manager/game-manager.component';
 import { RulesComponent } from './components/rules/rules.component';
 import { AboutComponent } from './components/about/about.component';
 
 export const routes: Routes = [
-    { path: 'main', component: LandingPageComponent },
+    { path: 'main', component: GameManagerComponent },
     { path: 'rules', component: RulesComponent },
     { path: 'scores', component: ScoresComponent },
     { path: 'about', component: AboutComponent },
