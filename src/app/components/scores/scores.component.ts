1a2f8af54418c61c35ac41cb7bc9e742188ef87d
RyazanGrove <53538807+RyazanGrove@users.noreply.github.com>
2024-08-28 22:16:07 +0300

    implement displaying of scores

--- src/app/components/scores/scores.component.ts
+++ src/app/components/scores/scores.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { ScoreService } from '../../services/score.service';
+import { Score } from '../../models/score';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-scores',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './scores.component.html',
   styleUrl: './scores.component.scss'
 })
 export class ScoresComponent {
 
+  scores: Score[] = [];
+
+  constructor(private scoreService: ScoreService) {}
+
+  ngOnInit(): void {
+    this.loadScores();
+  }
+
+  loadScores(): void {
+    this.scoreService.getScores().subscribe((scores: Score[]) => {
+      this.scores = scores;
+    },
+    (error) => {
+      console.error('Error fetching scores', error);
+    })
+  }
 }
