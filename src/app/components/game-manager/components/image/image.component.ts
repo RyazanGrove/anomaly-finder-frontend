5399f2a5a389ece0a13c37b415c36eaab6ed648a
RyazanGrove <53538807+RyazanGrove@users.noreply.github.com>
2024-08-31 00:13:33 +0300

    show circle on correct target

--- src/app/components/game-manager/components/image/image.component.ts
+++ src/app/components/game-manager/components/image/image.component.ts
@@ -16,10 +16,9 @@ export class ImageComponent implements OnChanges {
   @Output() targetFound = new EventEmitter<void>();
 
   image: string | undefined;
-  clickCoords: { x: number, y: number } | null = null;
-
-  boundingRectLeft = 0;  
-  boundingRectTop = 0;  
+  circleCenterX = 0;
+  circleCenterY = 0;
+  showTarget = false;
 
   constructor(private imageService: ImageService) { }
 
@@ -27,30 +26,44 @@ export class ImageComponent implements OnChanges {
     this.imageService.loadImage(this.data.fileName).subscribe(image => {
       this.image = image;
     })
+    this.showTarget = false;
   }
 
   onImageClick(event: MouseEvent): void {
     const imageElement = event.target as HTMLImageElement;
 
     const boundingRect = imageElement.getBoundingClientRect();
-    this.boundingRectLeft = boundingRect.left;
-    this.boundingRectTop = boundingRect.top;
-    const x = event.clientX - this.boundingRectLeft;
-    const y = event.clientY - this.boundingRectTop;
+    const x = event.clientX - boundingRect.left;
+    const y = event.clientY - boundingRect.top;
 
-    this.clickCoords = {
-      x: x,
-      y: y
-    }
-
-    this.checkForFoundTarget(x,y);
+    this.checkForFoundTarget(x,y, imageElement);
   }
 
-  checkForFoundTarget(x: number, y: number){
+  checkForFoundTarget(x: number, y: number, imageElement: any){
     const target = this.data.target;
 
-    if(target.xMin <= x && x <= target.xMax && target.yMin <= y && target.yMax){
+    if(target.xMin <= x && x <= target.xMax && target.yMin <= y && y <= target.yMax){
+      this.showTarget = true;
+      
+      // User can scale or move picture. Important to adjust position every time
+      const answer = this.getImagePosition(imageElement);
+      this.circleCenterX = (target.xMin + target.xMax) / 2 + answer.x - 35; // offset x to center circle on target
+      this.circleCenterY = (target.yMin + target.yMax) / 2 + answer.y - 35; // offset y to center circle on target
+
       this.targetFound.emit();
     }
+  }
+
+  getImagePosition(element: any) {
+    var xPosition = 0;
+    var yPosition = 0;
+
+    while(element) {
+        xPosition += (element.offsetLeft - element.scrollLeft + element.clientLeft);
+        yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+        element = element.offsetParent;
+    }
+
+    return { x: xPosition, y: yPosition };
   }
 }
