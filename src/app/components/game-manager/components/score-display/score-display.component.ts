f0223a9e5f1e6c0c18e7b08dd34b476f064da118
RyazanGrove <53538807+RyazanGrove@users.noreply.github.com>
2024-08-31 01:27:27 +0300

    implement score-display component

--- src/app/components/game-manager/components/score-display/score-display.component.ts
+++ src/app/components/game-manager/components/score-display/score-display.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-score-display',
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './score-display.component.scss'
 })
 export class ScoreDisplayComponent {
+
+  @Input() score = 0;
 
 }
