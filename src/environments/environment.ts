5d880b96f671ff46053c15be961a729c5072e027
RyazanGrove <53538807+RyazanGrove@users.noreply.github.com>
2024-08-31 01:30:52 +0300

    update version to 0.1.2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    version: '0.1.1',
+    version: '0.1.2',
     // Endpoints
     baseUrl: 'http://localhost:8080/',
     imageInfoEndpoint: 'image/info/',
