b60890a99a6d61ca06763234ced1ff1b21fe0e6b
RyazanGrove <53538807+RyazanGrove@users.noreply.github.com>
2024-08-31 16:43:31 +0300

    update rules component

--- src/app/components/rules/rules.component.ts
+++ src/app/components/rules/rules.component.ts
@@ -17,8 +17,13 @@ export class RulesComponent {
     "3. The image will be filled with distractions, making Wally hard to spot.",
     "4. Use your mouse or touch screen to navigate through the image and click on Wally when you find him.",
     "5. You can zoom in and out on the image to get a better view of specific areas.",
-    "6. Once you find Wally, the game will confirm your selection, and you will be given a score based on how quickly you found him.",
-    "7. The game can have multiple levels, with Wally becoming progressively harder to find as you advance.",
+    "6. Once you find Wally, the game will confirm your selection showing a red circle around Wally, and you will be given a score based on how quickly you found him.",
+    "7. The game has multiple images, and the sequence is generated automatically.",
+    //"7. The game can have multiple levels, with Wally becoming progressively harder to find as you advance.", // will be implemented later
     "8. Enjoy the game and try to improve your time with each level!"
+  ];
+  scores : string[] = [
+    "1. You get 1000 points every time you find Wally in the image.",
+    "2. Additionally you get 10,000 points minus 100 points for every second you spend searching for Wally, but not less than 0."
   ];
 }
